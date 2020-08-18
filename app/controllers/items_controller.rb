af41105a40c0d537cbfe797e18f375ffecb60719
takuya-kinoshita20160224 <takuyakinoshita20160224@gmail.com>
2020-08-18 13:04:33 +0900

    商品編集機能実装の修正１回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, only: :new
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -18,7 +19,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -28,13 +28,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :text, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -2,8 +2,6 @@ window.addEventListener('load', function() {
   const price = document.getElementById("price")
   const add_tax_price = document.getElementById("add-tax-price")
   const profit = document.getElementById("profit")
-
-  
   price.addEventListener('input',function(){
     add_tax_price.innerHTML = (price.value * 0.1).toLocaleString();
     profit.innerHTML = (price.value - (price.value * 0.1)).toString().replace(/(\d)(?=(\d{3})+$)/g , '$1,');
