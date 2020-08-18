57755756108d159ef6dae0e0cd2216094676dfd7
takuya-kinoshita20160224 <takuyakinoshita20160224@gmail.com>
2020-08-18 15:00:43 +0900

    商品編集機能実装の修正３回目（アップデート時のエラーハンドリング）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render "edit"
+    end
   end
 
   def show
