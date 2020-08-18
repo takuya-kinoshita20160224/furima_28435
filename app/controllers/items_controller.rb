59c96f30351b0fa30501a915bbf7be736719e9af
takuya-kinoshita20160224 <takuyakinoshita20160224@gmail.com>
2020-08-18 12:10:35 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,16 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to item_path
+  end
+
   def show
     @item = Item.find(params[:id])
   end
